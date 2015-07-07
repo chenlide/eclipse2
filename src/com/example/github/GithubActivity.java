9c1f2dfb95fc9966f45c565c781643899d524aae
Administrator <Administrator@10.10.121.149>
2015-07-07 19:38:25 +0800

    test

--- src/com/example/github/GithubActivity.java
+++ src/com/example/github/GithubActivity.java
@@ -9,6 +9,7 @@ public class GithubActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		//ĞŞ¸ÄÁËgithub
 	
 	}
 }
