f694e7053844051e44bdbb5e0bb1a2edf84d854b
Administrator <Administrator@10.10.121.149>
2015-07-07 19:58:05 +0800

    github 提交

---
+++ src/com/example/github/GithubActivity.java
@@ -0,0 +1,21 @@
+package com.example.github;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class GithubActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		//修改了github
+
+		// the code is from master
+
+		//开分支aaaaaaa
+		setContentView(null);
+
+	
+	}
+}
