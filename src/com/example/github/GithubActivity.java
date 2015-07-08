c3efa166b904b94a388ec0aa889c0317fe4eff7d
rade.chan <29771528@qq.com>
2015-07-08 10:58:38 +0800

    提交

--- src/com/example/github/GithubActivity.java
+++ src/com/example/github/GithubActivity.java
@@ -9,11 +9,9 @@ public class GithubActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		//ÐÞ¸ÄÁËgithub
 
 		// the code is from master
 
-		//¿ª·ÖÖ§aaaaaaa
 		setContentView(null);
 		// the code from remote;
 		// test again		
